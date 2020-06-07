c9520742e72ddc65d5ba75f4710c2c8491adf1de
Beyar <beyar-123@hotmail.com>
2020-06-07 17:30:31 +0200

    Added function for generating template

--- src/index.ts
+++ src/index.ts
@@ -5,69 +5,77 @@ import './style/404.scss'
 import { getNews, Result } from './parser'
 
 /**
- * @param {string} elementId
+ * @param {HTMLElement} elementId
  * @param {string} content
  */
-function injectContent (elementId: string, content: string): void {
-  document.getElementById(elementId).innerText = content
+function injectContent (elementId: HTMLElement, content: string): void {
+  elementId.innerHTML += content
 }
 
-const templateStructure = `<div class="column is-three-fifths is-offset-one-fifth">
-  <div class="card">
-    <div class="card-content">
-      <div class="media">
-        <div class="media-content">
-          <div class="columns">
-            <div class="column">
-              <p class="title is-4" id="news_title">{{ NEWS_TITLE }}</p>
+/**
+ * @param {string} NEWS_TITLE
+ * @param {string} NEWS_CREATED
+ * @param {string} NEWS_UPDATED
+ * @param {string} NEWS_DESCRIPTION
+ * @param {string} NEWS_TYPE
+ * @param {string} NEWS_TAG
+ * @param {string} NEWS_AUTHOR
+ * @returns
+ */
+function createArticle (NEWS_TITLE: string,
+  NEWS_CREATED: string,
+  NEWS_UPDATED: string,
+  NEWS_DESCRIPTION: string,
+  NEWS_TYPE: string,
+  NEWS_TAG: string,
+  NEWS_AUTHOR: string) {
+  return `<div class="column is-three-fifths is-offset-one-fifth">
+      <div class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <div class="columns">
+                <div class="column">
+                  <p class="title is-4" id="news_title">${NEWS_TITLE}</p>
+                </div>
+                <div class="column">
+                  <p style="float: right" id="news_created">Skapad: ${NEWS_CREATED}</p>
+                  <br>
+                  <p style="float: right" id="news_updated"> Ändrad: ${NEWS_UPDATED}</p>
+                </div>
+              </div>
             </div>
-            <div class="column">
-              <p style="float: right" id="news_created">Skapad: {{ NEWS_CREATED }}</p>
-              <p style="float: right" id="news_updated">Ändrad: {{ NEWS_UPDATED }}</p>
-            </div>
+          </div>
+          <div class="content" id="news_content">
+            ${NEWS_DESCRIPTION}
+          </div>
+
+          <div class="container">
+            <span class="tag is-light">${NEWS_TYPE}</span>
+            <span class="tag is-dark">${NEWS_TAG}</span>
+          </div>
+
+          <div class="container" style="padding-top: 1rem; text-align: right;">
+            <p class="subtitle">${NEWS_AUTHOR}</p>
           </div>
         </div>
       </div>
-      <div class="content" id="news_content">
-        {{ NEWS_DESCRIPTION }}
-      </div>
-
-      <div class="container">
-        <span class="tag is-light">{{ NEWS_TYPE }}</span>
-        <span class="tag is-dark">{{ NEWS_TAG }}</span>
-      </div>
-
-      <div class="container" style="padding-top: 1rem; text-align: right;">
-        <p class="subtitle">{{ NEWS_AUTHOR }}</p>
-      </div>
-    </div>
-  </div>
-</div>`
-
-/**
- * @param {string} template
- * @param {RegExp} search
- * @returns {RegExpMatchArray}
- */
-function templateParser (template: string, search: RegExp): RegExpMatchArray {
-  const filter: RegExp = search
-  const matches: RegExpMatchArray = template.match(filter)
-
-  return matches
+    </div>`
 }
 
-getNews().then((value: Result): void => {
-  const pointOfInjection: RegExp = /{{2}\s[^ ]+\s}{2}/g
-  const stripCurlyBraces: RegExp = /({{2}\s)|(\s}{2})/g
-  const matchArray: RegExpMatchArray = templateParser(templateStructure, pointOfInjection)
-
-  const strippedArray: Array<string> = []
-  matchArray.forEach(element => {
-    strippedArray.push(element.replace(stripCurlyBraces, ''))
-  })
-
-  console.log(strippedArray)
+getNews().then((value: Result): Array<string> => {
   console.log(value)
-}).then((value: any): void => {
-  const app: HTMLElement = document.getElementById('app')
+
+  const articles: Array<string> = []
+  for (let start = 0; start <= value.newsCount.length - 1; start++) {
+    articles.push(
+      createArticle(value.title, value.created, value.updated, value.description, value.tagType, value.tagTitle, value.author)
+    )
+  }
+  return articles
+}).then((articles: any): void => {
+  const app: HTMLElement = document.getElementById('root')
+  articles.forEach(article => {
+    injectContent(app, article)
+  })
 })
